f0eb318a16d9276901b9cd61aa8ed2f57988893d
Cameron Sequeira-Hogg <cameronsequeirahogg@gmail.com>
2021-04-07 10:50:46 -0400

    Add files via upload
    Added V1 of the notebook, and Finalized LR file that calculates L2 and OLS and calculates RMSE.

---
+++ LR.m
@@ -0,0 +1,47 @@
+clc
+clear
+
+%Load Sample Dataset
+data = readmatrix('clean_data');
+
+%Set Lambda for Ridge Regression Regularization 
+ridge = 15;
+
+%Load training dataset
+X = readmatrix('X_train');
+%Add intercept
+X = [ones(length(X),1) X];
+y = readmatrix('y_train');
+
+%Load testing dataset
+X_test = readmatrix('X_test');
+%Add intercept
+X_test = [ones(length(X_test),1) X_test];
+y_test = readmatrix('y_test');
+
+% %OLS: Solving the Normal Equations Directly
+% L = inv(X'*X);
+% beta_OLS = L*X'*y;
+
+%OLS:Solving with QR Decomposition 
+[Q,R] = qr(X,0);
+beta_OLS = R\(Q'*y);
+
+%OLS: RMSE
+y_pred = X_test*beta_OLS;
+error = y_test - y_pred;
+squared_error = error.^2;
+sqrt(mean(squared_error))
+
+%Ridge Regression: Solving the Normal Equations Directly
+L = inv(X'*X+ridge*eye(length(X'*X)));
+beta_RR = L*X'*y;
+
+%Ridge Regression: RMSE
+y_pred = X_test*beta_RR;
+error = y_test - y_pred;
+squared_error = error.^2;
+sqrt(mean(squared_error))
+
+
+
